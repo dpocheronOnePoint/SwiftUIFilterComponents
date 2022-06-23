f7ebfb7543ced2284dc14126c6faee87734cf3b3
your_username <your_email@gmail.com>
2022-06-23 07:15:41 +0200

    Initial Commit

---
+++ FilterViewProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FilterViewProject
+//
+//  Created by Dimitri POCHERON on 23/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FilterViewProject/FilterViewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilterViewProjectApp.swift
+//  FilterViewProject
+//
+//  Created by Dimitri POCHERON on 23/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct FilterViewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
